da1809e69bf33de21a53ae42a419435a9dadf32a
Shuana <61404361+shuanaongithub@users.noreply.github.com>
2020-10-04 12:37:59 +0100

    Upgraded to discord.py 1.5.0
    Added intents

--- bot.py
+++ bot.py
@@ -1,44 +1,54 @@
 import discord
+import asyncio
+import random
+import os
+import time
+import requests
+import aiohttp
+import urllib
 import json
 import discord
-
+import sys
+import inspect
+import traceback
+import datetime
+import platform
+import pip
+import io
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
-
 def get_prefix(bot, message):
-    prefixes = ['t/']
+    prefixes = ['w/', 'world ']
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
-bot = commands.Bot(command_prefix=get_prefix, description='Discord Bot Made For All', case_insensitive=True)
+bot = commands.Bot(command_prefix=get_prefix, description='Discord Bot Made For All', case_insensitive=True, intents=discord.Intents.all())
 bot.remove_command("help")
 
-
 initial_extensions = ['cogs.owner', 'cogs.ping', 'cogs.help',
- 'cogs.info', 'cogs.economy', 'cogs.fun', 'cogs.mod', 'cogs.logging']
+ 'cogs.info', 'cogs.economy', 'cogs.fun', 'cogs.mod', 'cogs.statcord', 'cogs.iad', 'cogs.logging']
 
 
 if __name__ == '__main__':
-	for extension in initial_extensions:
-		bot.load_extension(extension)
+    for extension in initial_extensions:
+        bot.load_extension(extension)
 
 
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name="w/help")
     )
-    print(
-        f"""        ----------------
-        Bot is ready!
-        ---------------
-        Author: Sean/Worlds Owner
-        ---------------
-        Logged in as: {bot.user.name}
-        ---------------
-        Current Version: 433634.128947
-        ---------------"""
-    )
-
+    print("""Status:
+    ----------------
+    Bot is ready!
+    ---------------
+    Author: Sean, Fxcilities, and CatNowBlue
+    ---------------
+    Logged in as: {bot.user.name}
+    ---------------
+    Current Version: 433634.128947
+    ---------------
+    """)
 
 @bot.command(aliases=["firstmessage"], help="Pull first sent message in a channel.")
 @commands.has_permissions(manage_messages=True)
@@ -66,29 +76,8 @@ async def fm(ctx, channel: discord.TextChannel=None):
 
 @fm.error
 async def fm_error(ctx, error):
-	if isinstance(error, commands.CheckFailure):
-		await ctx.send(f':regional_indicator_x: Sorry {ctx.author.mention} You Dont Have `manage messages` Permission')
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if bot.user in message.mentions:
-        embed = discord.Embed(
-        title='', description=f'Here Are My Prefixes: `w/`, `world `' , colour=message.author.color)
-        embed.set_footer(text="Type <prefix>help for more info.")
-        await message.channel.send(embed = embed)
-    await bot.process_commands(message)
-
-
-@bot.event
-async def on_message(message: discord.Message) -> None:
-    with open("blacklisted.json") as file:
-        blacklisted_users = json.load(file)
-        if message.author.id in blacklisted_users["blacklisted"]:
-            return
-
-    await bot.process_commands(message)
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send(f':regional_indicator_x: Sorry {ctx.author.mention} You Dont Have `manage messages` Permission')
 
 
 @bot.command(help="[Nsfw], Screenshot a website.")
@@ -101,7 +90,7 @@ async def screenshot(ctx, site):
         embed=discord.Embed(title="Nsfw Only!", timestamp=ctx.message.created_at, color=ctx.author.color)
         embed.set_image(url=f"https://media.discordapp.net/attachments/265156286406983680/728328135942340699/nsfw.gif")
         await ctx.send(embed=embed)
-	
+
 @bot.event
 async def on_command_error(ctx, error):
     if ctx.author.id == 662334026098409480 or ctx.author.id == 393859835331870721:
@@ -110,4 +99,4 @@ async def on_command_error(ctx, error):
         embed.set_thumbnail(url=bot.user.avatar_url)
         await channels.send(embed=embed)
 
-bot.run("no token for u", bot=True, reconnect=True)
+bot.run("NzAwMjkyMTQ3MzExNTQyMjgy.Xpgz4g.AUVySVkyceqMi_6U9ZRg6FqiY9s", bot=True, reconnect=True)
